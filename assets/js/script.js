55b5d2da740be756f882ac094c5a957df3404903
kristafrancis721 <kristafrancis08@yahoo.com>
2022-07-03 18:37:23 -0400

    added scriptjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,71 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const timeElement = document.getElementById('current-time');
+const maxTime = 75;
+var timer;
+var currentTime;
 
-startButton.addEventListener('click', startGame)
+var currentScore;
+
+startButton.addEventListener('click', startGame);
 
 
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+    console.log('Started');
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+    startTimer();
 
 }
+
+function endGame() {
+clearInterval(timer);
+//todo
+//save initials and high score
+}
+
+function updateTimer() {
+    currentTime--;
+    timeElement.innerText = currentTime;
+    
+    if (currentTime <= 0) {
+        endGame();
+
+    }
+}
+
+function startTimer() {
+    currentTime = maxTime;
+    timer = setInterval(updateTimer, 1000);
+    timeElement.text = currentTime;
+}
+
+function saveScore(){
+//todo
+}
+
+function showScore() {
+    //todo
+}
+
+function resetScore() {
+    //todo
+}
+
+
+funs
 
 function setNextQuestion() {
-    resetState()
+    //resetState()
 
 }
+
+//score functions
+//todo
+
+//save
 
 function showQuestion(question) {
     questionContainerElement.innerText = question.question
@@ -26,6 +75,7 @@ function showQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
+    
         button.addEventListener('click', selectAnswer)
         answerButtonElement.appendChild(button)
     })
@@ -49,31 +99,35 @@ const questions = [
            { text: '""', correct: false },
            { text: '[]', correct: true },
            { text: '{}', correct: false },
-           { text: '()', correct: false },
+           { text: '()', correct: false }
        ]
-       
+    },
+    {
        question: 'To declare the end of a statement use',
        answer: [
            { text: ';', correct: true },
            { text: ':', correct: false },
            { text: '.', correct: false },
-           { text: '/', correct: false },
+           { text: '/', correct: false }
        ]
-
-       question: 'What does a string refer to?'
+    },
+    {
+       question: 'What does a string refer to?',
        answer: [
            { text: 'numbers', correct: false },
            { text: 'text', correct: true },
            { text: 'thread', correct: false },
-           { text: 'none of the above', correct: false },
+           { text: 'none of the above', correct: false }
        ]
+    },
+    {
 
        question: 'What git command creates a new branch', 
        answer: [
            { text: 'git checkout -b', correct: true },
            { text: 'git commit -b', correct: false },
            { text: 'git checkout -m', correct: false },
-           { text: 'git status', correct: false },
+           { text: 'git status', correct: false }
        ]
-    }
+    },
 ]
